fca016726c70004f25d6ac3545a52f0e1231e308
Вячеслав Коханный <xoxluwa1@yandex.com>
2021-11-20 22:11:24 +0500

    remove

---
+++ src/components/Articles.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Card from './Card';
+
+const Articles = ({ articles }) => {
+  const leftArticlesCount = Math.ceil(articles.length / 5);
+  const leftArticles = articles.slice(0, leftArticlesCount);
+  const rightArticles = articles.slice(leftArticlesCount, articles.length);
+
+  return (
+    <div>
+      <div>
+        <div>
+          {leftArticles.map((article, i) => {
+            return (
+              <Card article={article} key={`article__left__${article.slug}`} />
+            );
+          })}
+        </div>
+        <div>
+          <div>
+            {rightArticles.map((article, i) => {
+              return (
+                <Card
+                  article={article}
+                  key={`article__left__${article.slug}`}
+                />
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Articles;
---
+++ src/components/Card.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Link from 'next/link';
+import Image from './image';
+
+const Card = ({ article }) => {
+  return (
+    <Link as={`/article/${article.slug}`} href='/article/[id]'>
+      <a>
+        <div>
+          <div>
+            {/* eslint-disable-next-line jsx-a11y/alt-text */}
+            <Image image={article.image} />
+          </div>
+          <div>
+            <p id='category'>
+              {article.category.name}
+            </p>
+            <p id='title'>
+              {article.title}
+            </p>
+          </div>
+        </div>
+      </a>
+    </Link>
+  );
+};
+
+export default Card;
