70741a918aa674af806e45e5aa30a9e20d5f24ac
Вячеслав Коханный <xoxluwa1@yandex.com>
2021-11-22 11:30:43 +0500

    add URL for shareButtons

--- src/components/shareButtons.js
+++ src/components/shareButtons.js
@@ -12,35 +12,34 @@ import {
   VKIcon,
 } from 'react-share';
 
-export const getURL = (path = '') =>
-  {console.log(process.env); return `${process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000'}${path}`};
+const URL = 'https://kohanniy.vercel.app';
 
 export const FacebookShare = withRouter(({ iconProps, router, ...props }) => (
-  <FacebookShareButton url={getURL(router.pathname)} {...props}>
+  <FacebookShareButton url={`${URL}${router.pathname}`} {...props}>
     <FacebookIcon {...iconProps} /> Поделиться
   </FacebookShareButton>
 ));
 
 export const LinkedinShare = withRouter(({ router, iconProps, ...props }) => (
-  <LinkedinShareButton url={getURL(router.pathname)} {...props}>
+  <LinkedinShareButton url={`${URL}${router.pathname}`} {...props}>
     <LinkedinIcon {...iconProps} /> Линкануть
   </LinkedinShareButton>
 ));
 
 export const TelegramShare = withRouter(({ router, iconProps, ...props }) => (
-  <TelegramShareButton url={getURL(router.pathname)} {...props}>
+  <TelegramShareButton url={`${URL}${router.pathname}`} {...props}>
     <TelegramIcon {...iconProps} /> Отправить
   </TelegramShareButton>
 ));
 
 export const TwitterShare = withRouter(({ router, iconProps, ...props }) => (
-  <TwitterShareButton url={getURL(router.pathname)} {...props}>
+  <TwitterShareButton url={`${URL}${router.pathname}`} {...props}>
     <TwitterIcon {...iconProps} /> Твитнуть
   </TwitterShareButton>
 ));
 
 export const VKShare = withRouter(({ router, iconProps, ...props }) => (
-  <VKShareButton url={getURL(router.pathname)} {...props}>
+  <VKShareButton url={`${URL}${router.pathname}`} {...props}>
     <VKIcon {...iconProps} /> Поделиться
   </VKShareButton>
 ));
